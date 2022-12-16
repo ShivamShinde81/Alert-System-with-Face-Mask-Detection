59253d07953f734727a7163696a38072c9bd8b11
ShivamShinde81 <73120052+ShivamShinde81@users.noreply.github.com>
2022-12-16 23:29:05 +0530

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -2,12 +2,9 @@ import smtplib, ssl
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-# sender_email = input("(Note: https://myaccount.google.com/lesssecureapps should be on for this email)\nType your gmail Sender email : ")  # Enter your address
-# password = input("Type your gmail password: ")
-# receiver_email = input("Type your Receiver email: ")
-sender_email = "shivamshinde@mitaoe.ac.in"
-password= "Shivam@2806"
-receiver_email = "shivamshinde240@gmail.com"
+sender_email = input("(Note: https://myaccount.google.com/lesssecureapps should be on for this email)\nType your gmail Sender email : ")  # Enter your address
+password = input("Type your gmail password: ")
+receiver_email = input("Type your Receiver email: ")
 def sendEmail(msg,receiver_email=receiver_email):
 
 	message = 'Subject: {}\n\n{}'.format("Facemask Notifier", msg)
